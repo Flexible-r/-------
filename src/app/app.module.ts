9e422e1258e4ba11aee36a79d20f7f1dc0f34dc5
Nick <nikitamm59rus@gmail.com>
2021-02-02 23:48:41 +0400

    basket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BasketComponent } from './basket/basket.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasketComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
