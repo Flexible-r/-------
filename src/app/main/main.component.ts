299e68cd7d843872a8a6d9cb4f468d0c872e9879
Flex <artem.vagin1945@gmail.com>
2021-02-03 22:31:28 +0300

    дхх

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from '../basket.service';
 import { ModelsService } from '../models.service';
-
+interface Alert {
+  type: string;
+  message: string;
+}
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -16,4 +19,6 @@ export class MainComponent {
   increase(product: any) {
     this.basketService.addToBaskets(product);
   }
+  
 }
+export class NgbdAlertCustom {}
